f83b1eeb5d8803d14e889a09b707daa07988ac41
aryanssd <aryan.collaborate@gmail.com>
2021-12-18 15:41:26 +0330

    Seventeenth-exercices-for-loop

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-for(var i = 0; i <= 100; i += 2){
-    console.log(i)
+var p1 = 1000
+var p2 = 1000
+var p3 = 1000
+var p4 = 1000
+var p5 = 1000
+
+var sum = p1 + p2 + p3 + p4 + p5
+
+for(i = 0; i < 5; i++){
+    console.log(sum);
 }
