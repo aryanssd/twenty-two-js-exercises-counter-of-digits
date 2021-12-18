abebc1ffd04be890bc0f53ca50a4d09117a56baf
aryanssd <aryan.collaborate@gmail.com>
2021-12-18 15:55:00 +0330

    Seventeenth-exercices-for-loop-way-two

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-var p1 = 1000
-var p2 = 1000
-var p3 = 1000
-var p4 = 1000
-var p5 = 1000
+var p1 = Number(prompt("Please enter the price of product..."))
+var p2 = Number(prompt("Please enter the price of product..."))
+var p3 = Number(prompt("Please enter the price of product..."))
+var p4 = Number(prompt("Please enter the price of product..."))
+var p5 = Number(prompt("Please enter the price of product..."))
 
 var sum = p1 + p2 + p3 + p4 + p5
 
-for(i = 0; i < 5; i++){
-    console.log(sum);
+for (i = 0; i <= 5; i++){
 }
+
+alert(sum)
